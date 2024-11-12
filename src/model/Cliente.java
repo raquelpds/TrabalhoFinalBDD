6ba252dc87ba73f790cd08b51014695e378b7c42
raquelpds <raquelpds09@gmail.com>
2024-11-12 20:06:59 -0300

    Alteração na classe cliente e funcionário

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,16 @@
 package model;
 
-public class Cliente {
+import java.time.LocalDate;
+import java.util.Locale;
+
+abstract class Cliente {
+    //atributos:
+    int id;
+    String nome, cpf, telefone, endereco;
+    LocalDate dataNascimento;
+    //métodos
+     abstract boolean login(String senha);
+     abstract void logout();
+     abstract String consultarDados();
+
 }
--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,4 +1,36 @@
 package model;
 
-public class Funcionario {
+public class Funcionario extends Cliente {
+    //atributos
+    private String codigoFuncionario;
+    private String cargo;
+    private String senha;
+
+    //Construtor
+    public Funcionario (String codigoFuncionario, String cargo, String senha){
+        this.codigoFuncionario = codigoFuncionario;
+        this.cargo = cargo;
+        this.senha = senha;
+    }
+
+    //métodos
+    void abrirConta( Conta conta){
+
+    }
+
+
+    @Override
+    boolean login(String senha) {
+        return false;
+    }
+
+    @Override
+    void logout() {
+
+    }
+
+    @Override
+    String consultarDados() {
+        return "";
+    }
 }
