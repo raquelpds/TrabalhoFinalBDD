c784f9211ae4f8d36aa97c7f7e90bcf37b6d7119
raquelpds <raquelpds09@gmail.com>
2024-11-13 15:59:13 -0300

    Desenvolvimento da classe Conta com seus métodos já inicializados

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -1,4 +1,67 @@
 package model;
 
-public class Conta {
+abstract class Conta {
+    private int numero;
+    private String agencia;
+    private double saldo;
+    private Cliente cliente;
+
+    //construtor
+    public Conta(String agencia, Cliente cliente, int numero, double saldo) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    //métodos getters e setters
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    //métodos
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor){
+        if(this.saldo <= valor){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public double consultarSaldo(){
+        return saldo;
+    }
+
 }
