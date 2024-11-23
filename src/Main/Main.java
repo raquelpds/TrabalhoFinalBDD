e5bb2de0a5e9a747aaa49fe962612ef0caf44b94
raquelpds <raquelpds09@gmail.com>
2024-11-23 17:44:40 -0300

    alterações finais

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,7 +5,7 @@ import view.LoginView;
 public class Main {
 
     public static void main(String[] args) {
-        // Inicia a aplicação exibindo a TelaLogin
-        new LoginView().main(args);  // Chama o método main da classe LoginView
+        // Inicia a TelaLogin
+        new LoginView().main(args);  //
     }
 }
